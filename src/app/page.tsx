b4ec255a9c424c4ca13cce9ad409363f1a64d424
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-06-04 16:07:54 -0500

    creating adding animated text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { useState } from "react"
-import Image from 'next/image'
-import name from '@/assets/name.svg'
-import nameblack from '@/assets/nameblack.svg'
+// import Image from 'next/image'
+// import name from '@/assets/name.svg'
+// import nameblack from '@/assets/nameblack.svg'
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook"
 import { setColor } from "@/redux/slice/colorSlice"
+import style from '@/styles/rainbow.module.scss'
 export default function Home() {
   // const [background, setBackground] = useState('black');
   const background = useAppSelector((state) => state.color.background);
@@ -16,10 +17,18 @@ export default function Home() {
       setIndex(nextIndex);
       dispatch(setColor(colors[nextIndex]));
   };
+  const listItems = Array.from({ length: 3 }, (_, i) => (
+    <li key={i} className={`${background === 'black' || background === 'gray' ? 'text-white': 'text-black' } `}>manuel blanco</li>
+  ));
   return (
     <main onClick={changeBackground} className={`flex min-h-screen  flex-col items-center justify-between p-1  transition-colors duration-500 `}  style={{ backgroundColor: background }}>
       <div className="m-auto " >
-        { background === 'black' || background === 'gray' ? <Image src={name} alt="name" width={350} height={302.42} /> : <Image src={nameblack} alt="name" width={350} height={302.42} /> } 
+        
+        <ul className={`${style.rainbowlist}`}>
+         {listItems}         
+        </ul>
+        {/* <h1 className={`${style.rainbow} ${background === 'black' || background === 'gray' ? 'text-white': 'text-black' }  ` } >manuel blanco</h1> */}
+        {/* { background === 'black' || background === 'gray' ? <Image src={name} alt="name" width={350} height={302.42} /> : <Image src={nameblack} alt="name" width={350} height={302.42} /> }  */}
       </div>
     </main>
   );
