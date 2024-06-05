fd5aa474c0d88a6af90e561f86c645948c73f913
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-06-04 21:59:19 -0500

    FallinText Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,46 @@
 'use client'
-import { useState } from "react"
-// import Image from 'next/image'
-// import name from '@/assets/name.svg'
-// import nameblack from '@/assets/nameblack.svg'
+import { useState, useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook"
 import { setColor } from "@/redux/slice/colorSlice"
 import style from '@/styles/rainbow.module.scss'
+import FallingTex from "@/components/FallingTex"
 export default function Home() {
   // const [background, setBackground] = useState('black');
   const background = useAppSelector((state) => state.color.background);
   const colors = ['black','yellow','white','gray'];
   const [index, setIndex] = useState(0);
+  const [animated, setAnimated] = useState(false);
   const dispatch = useAppDispatch();
   const changeBackground = () => {
       const nextIndex = (index + 1) % colors.length;
       setIndex(nextIndex);
       dispatch(setColor(colors[nextIndex]));
+      setAnimated(true);
   };
+
+  useEffect(() => {
+    if (animated) {
+      const timer = setTimeout(() => setAnimated(false),1500); // Duración de la animación
+      return () => clearTimeout(timer);
+    }
+  }, [animated]);
+  
+
   const listItems = Array.from({ length: 5 }, (_, i) => (
-    <li key={i} className={` ${background === 'black' || background === 'gray' ? 'text-white': 'text-black' }  `}>manuel blanco</li>
+    <li key={i} className={`${style.list} ${background === 'black' || background === 'gray' ? 'text-white': 'text-black' } ${animated ? style.animated : ''}  `}>manuel blanco</li>
   ));
   return (
-    <main onClick={changeBackground} className={`flex items-center min-h-screen   transition-colors duration-500 `}  style={{ backgroundColor: background }}>
-      <div className="m-auto">
-
+      <main onClick={changeBackground} className={`flex p-1 min-h-screen   transition-colors duration-500 `}  style={{ backgroundColor: background }}> 
         <ul className={`${style.rainbowlist}`}>
          {listItems}         
         </ul>
-       </div>      
-        {/* { background === 'black' || background === 'gray' ? <Image src={name} alt="name" width={350} height={302.42} /> : <Image src={nameblack} alt="name" width={350} height={302.42} /> }  */}
+        </main>
+    // <main onClick={changeBackground} className={`flex p-1 min-h-screen   transition-colors duration-500 `}  style={{ backgroundColor: background }}>
+    //   {/* <div className="flex justify-end items-end m-auto"> */}
+    //    {/* </div> */}
+    //   {/* <FallingTex></FallingTex> */}
+    //     {/* { background === 'black' || background === 'gray' ? <Image src={name} alt="name" width={350} height={302.42} /> : <Image src={nameblack} alt="name" width={350} height={302.42} /> }  */}
       
-    </main>
+    // </main>
   );
 }
---
+++ src/components/FallingTex.tsx
@@ -0,0 +1,19 @@
+import styles from '@/styles/rainbow.module.scss'
+
+const FallingTex = () => {
+  
+  return (
+    
+      <ul className={styles.rainbowlist}>
+        <li className={styles.list}>manuel白</li>
+        <li className={styles.list}>manuel白</li>
+        <li className={styles.list}>manuel白</li>
+        <li className={styles.list}>manuel白</li>
+        <li className={styles.list}>manuel白</li>
+
+      </ul>
+   
+  )
+}
+
+export default FallingTex
