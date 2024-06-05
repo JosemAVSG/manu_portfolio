5af2aaded4c86c09798eff22e60c9101a589b88e
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-06-04 22:36:00 -0500

    modularizando a falling text component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,26 @@
-'use client'
-import { useState, useEffect } from "react"
-import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook"
-import { setColor } from "@/redux/slice/colorSlice"
-import style from '@/styles/rainbow.module.scss'
-import FallingTex from "@/components/FallingTex"
+"use client";
+import { useState, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook";
+import { setColor } from "@/redux/slice/colorSlice";
+import style from "@/styles/rainbow.module.scss";
+import FallingTex from "@/components/FallingTex";
 export default function Home() {
-  // const [background, setBackground] = useState('black');
   const background = useAppSelector((state) => state.color.background);
-  const colors = ['black','yellow','white','gray'];
+  const colors = ["black", "yellow", "white", "gray"];
   const [index, setIndex] = useState(0);
-  const [animated, setAnimated] = useState(false);
   const dispatch = useAppDispatch();
   const changeBackground = () => {
-      const nextIndex = (index + 1) % colors.length;
-      setIndex(nextIndex);
-      dispatch(setColor(colors[nextIndex]));
-      setAnimated(true);
+    const nextIndex = (index + 1) % colors.length;
+    setIndex(nextIndex);
+    dispatch(setColor(colors[nextIndex]));
   };
-
-  useEffect(() => {
-    if (animated) {
-      const timer = setTimeout(() => setAnimated(false),1500); // Duración de la animación
-      return () => clearTimeout(timer);
-    }
-  }, [animated]);
-  
-
-  const listItems = Array.from({ length: 5 }, (_, i) => (
-    <li key={i} className={`${style.list} ${background === 'black' || background === 'gray' ? 'text-white': 'text-black' } ${animated ? style.animated : ''}  `}>manuel blanco</li>
-  ));
   return (
-      <main onClick={changeBackground} className={`flex p-1 min-h-screen   transition-colors duration-500 `}  style={{ backgroundColor: background }}> 
-        <ul className={`${style.rainbowlist}`}>
-         {listItems}         
-        </ul>
-        </main>
-    // <main onClick={changeBackground} className={`flex p-1 min-h-screen   transition-colors duration-500 `}  style={{ backgroundColor: background }}>
-    //   {/* <div className="flex justify-end items-end m-auto"> */}
-    //    {/* </div> */}
-    //   {/* <FallingTex></FallingTex> */}
-    //     {/* { background === 'black' || background === 'gray' ? <Image src={name} alt="name" width={350} height={302.42} /> : <Image src={nameblack} alt="name" width={350} height={302.42} /> }  */}
-      
-    // </main>
+    <main
+      onClick={changeBackground}
+      className={`flex p-1 min-h-screen   transition-colors duration-500 `}
+      style={{ backgroundColor: background }}
+    >
+      <FallingTex></FallingTex>
+    </main>
   );
 }
--- src/components/FallingTex.tsx
+++ src/components/FallingTex.tsx
@@ -1,17 +1,30 @@
-import styles from '@/styles/rainbow.module.scss'
+'use client'
+import style from '@/styles/rainbow.module.scss'
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '@/hooks/reduxHook'
 
 const FallingTex = () => {
-  
-  return (
-    
-      <ul className={styles.rainbowlist}>
-        <li className={styles.list}>manuel白</li>
-        <li className={styles.list}>manuel白</li>
-        <li className={styles.list}>manuel白</li>
-        <li className={styles.list}>manuel白</li>
-        <li className={styles.list}>manuel白</li>
+  const background = useAppSelector((state) => state.color.background);
+  const [animated, setAnimated] = useState(false);
 
-      </ul>
+  useEffect(() => {
+      setAnimated(true);
+  },[background])
+  useEffect(() => {
+    if (animated) {
+      const timer = setTimeout(() => setAnimated(false),2500); // Duración de la animación
+      return () => clearTimeout(timer);
+    }
+  }, [animated]);
+  
+
+  const listItems = Array.from({ length: 10 }, (_, i) => (
+    <li key={i} className={`${style.list} ${background === 'black' || background === 'gray' ? 'text-white': 'text-black' } ${animated ? style.animated : ''}  `}>manuel blanco</li>
+  ));
+  return (
+    <ul className={`${style.rainbowlist}`}>
+    {listItems}         
+   </ul>
    
   )
 }
