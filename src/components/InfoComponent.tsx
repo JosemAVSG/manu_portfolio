a1d9d6d6ccda5af1d8dcc18981030ac207bbf279
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-06-07 00:39:51 -0500

    styling infocomponent

--- src/components/InfoComponent.tsx
+++ src/components/InfoComponent.tsx
@@ -1,11 +1,25 @@
-
-
+import styles from "@/styles/infopage.module.scss";
 const InfoComponent = () => {
   return (
-    <div className="row-span-3">
-      <h1>Hola</h1>
+    <div className={styles.infocomponent}>
+      <div className="border-t-2 border-b-1 border-black row-span-1 ">
+        <p className="m-4 sm:text-lg md:text-xl overflow-y-auto ">
+          Hello and welcome to my website. My name is Manuel Blanco, and I am a
+          programmer based in Bogotá, Colombia.<br/> With a strong interest in
+          technology and culture, I work on a variety of self-commissioned
+          projects as well as for various clients to help them transform their
+          ideas into visual worlds. <br/><br/>If you would like to work or get in touch
+          with me, please don{"'"}t hesitate to send me an email :{")"} 
+        </p>
+      </div>
+      <div className="border-t-2 border-black row-span-1">
+        <h2 className="m-4">correo</h2>
+      </div>
+      <div className="border-t-2  broder-gray-500 row-span-1">
+        <h2 className="m-4">redes</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default InfoComponent;
