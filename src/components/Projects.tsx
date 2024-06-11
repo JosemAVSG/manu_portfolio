33c453e721103ed7064717400c4b9d038819eff2
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-06-11 18:38:27 -0500

    adding some changes to styles of navbar and projects menu

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,21 @@
 import styles from "@/styles/projects.module.scss";
 import { faTags } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+
 const Projects = () => {
   return (
     <>
       <div className={styles.projects}>
         <ul className={styles.list}>
+          
           <li>
-            <div className={styles.title}> 
+            <div className={`font-sequel ${ styles.title} `}> 
            <h2>Project</h2>  
             <FontAwesomeIcon  icon={faTags} width={10} />
             </div>
-            <ul className={styles.techlist}>
+            <ul className={` font-sequel ${styles.techlist }`}>
               <li>html</li>
               <li>css</li>
               <li>javascript</li>
